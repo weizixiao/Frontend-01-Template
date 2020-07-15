8bb22fa8cab597d996e8423d70cbc469a1e69419
huangweihua <871919546@qq.com>
2020-07-15 22:34:28 +0800

    week14

--- week14/createElement.js
+++ week14/createElement.js
@@ -1,79 +1,79 @@
 
 export function create (Cls, attributes, ...children) {
-    let o
+    let o;
     if (typeof Cls === 'string') {
-      o = new Wrapper(Cls)
+        o = new Wrapper(Cls);
     }else {
-      o = new Cls({
-        timer: {}
-      })
+        o = new Cls({
+            timer: {}
+        })
     }
   
     for (const key in attributes) {
-      o.setAttribute(key, attributes[key])
+        o.setAttribute(key, attributes[key]);
     }
     let visit = (children) => {
-      for (const child of children) {
-        if (typeof child === 'string') {
-          child = new Text(child)
+        for (const child of children) {
+            if (typeof child === 'string') {
+                child = new Text(child);
+            }
+    
+            if (Array.isArray(child)) {
+                visit(child);
+                continue;
+            }
+            o.appendChild(child);
         }
-  
-        if (Array.isArray(child)) {
-          visit(child)
-          continue
-        }
-        o.appendChild(child)
-      }
     }
      
     visit(children)
     return o
-  }
+}
 
-  export class Wrapper {
+export class Wrapper {
     constructor (type) {
-      this.children = []
-      this.root = document.createElement(type)
+        this.children = [];
+        this.root = document.createElement(type);
     }
     setAttribute (name, value) {
-      this.root.setAttribute(name, value)
+        this.root.setAttribute(name, value);
     }
     appendChild (child) {
-      this.children.push(child)
+        this.children.push(child);
     }
     addEventListener (...arg) {
-      this.root.addEventListener(...arg)
+        this.root.addEventListener(...arg);
     }
     mountTo (parent) {
-      parent.appendChild(this.root)
-      for (let child of this.children) {
-        child.mountTo(this.root)
-      }
+        parent.appendChild(this.root);
+        for (let child of this.children) {
+            child.mountTo(this.root);
+        }
     }
-  }
-  export class Text {
+}
+export class Text {
     constructor (text) {
-      this.root = document.createTextNode(text)
+        this.root = document.createTextNode(text);
     }
     mountTo (parent) {
-      parent.appendChild(this.root)
+        parent.appendChild(this.root);
     }
-  }
-  export class Div {
+}
+export class Div {
     constructor (config) {
-      this.children = []
-      this.root = document.createElement('div')
+        this.children = [];
+        this.root = document.createElement('div');
     }
     setAttribute (name, value) {
-      this.root.setAttribute(name, value)
+        this.root.setAttribute(name, value);
     }
     appendChild (child) {
-      this.children.push(child)
+        this.children.push(child);
     }
     mountTo (parent) {
-      parent.appendChild(this.root)
-      for (let child of this.children) {
-        child.mountTo(this.root)
-      }
+        parent.appendChild(this.root);
+        for (let child of this.children) {
+            child.mountTo(this.root);
+        }
     }
-  }
+}
