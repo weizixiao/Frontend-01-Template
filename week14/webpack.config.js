977db49e458ecc8ae3f6591d066ddfe6b53bf834
huangweihua <871919546@qq.com>
2020-07-22 16:42:51 +0800

    week15

--- week14/webpack.config.js
+++ week14/webpack.config.js
@@ -1,30 +1,36 @@
 const path = require("path");
 
 module.exports = {
-  entry: "./main.js",
-  mode: "development",
-  optimization: {
-    minimize: false,
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        use: {
-          loader: "babel-loader",
-          options: {
-            presets: ["@babel/preset-env"],
-            plugins: [
-              [
-                "@babel/plugin-transform-react-jsx",
-                {
-                  pragma: "create",
-                },
-              ],
-            ],
-          },
-        },
-      },
-    ],
-  },
+    entry: "./main.js",
+    mode: "development",
+    optimization: {
+        minimize: false,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: ["@babel/preset-env"],
+                        plugins: [
+                        [
+                            "@babel/plugin-transform-react-jsx",
+                            {
+                                pragma: "create",
+                            }
+                        ]
+                        ]
+                    }
+                }
+            },
+            {
+                test: /\.view$/,
+                use: {
+                    loader: path.resolve(__dirname, "./myLoader.js")
+                }
+            }
+        ]
+    }
 };
